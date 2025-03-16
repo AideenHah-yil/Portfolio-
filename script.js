aecc440314979576d65f98787e2b267feea76c23
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2025-03-16 06:25:23 +0300

    working on the js file

---
+++ script.js
@@ -0,0 +1,17 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+    let modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
